dfa593ec110081cdf72109eb2cdaa196a2e94259
juliannparra99 <julianfranco.parra@gmail.com>
2024-08-02 03:52:44 -0300

    Added: 10.10.3- new info into the chat bot

--- src/components/experience-section/Experience.jsx
+++ src/components/experience-section/Experience.jsx
@@ -3,24 +3,25 @@ import Image from "next/image";
 
 const workexperiences = [
   {
-    "position": "Frontend/ Backend Developer",
-    "company": "Solar T.S",
-    "company_link": "",
-    "project": "Customer Relationship Management, Landing Page, Content Management System",
-    "skills": "TypeScript, React.js, Next.js, Express, MySQL",
-    "period": "May 2023 - September-2023",
-    "desc": `evelopment of a comprehensive Customer Relationship Management (CRM) system, a captivating Landing Page, and a dynamic Content Management System (CMS). Employing TypeScript, React.js, Next.js, and Express, I played a pivotal role in crafting these solutions. With a focus on delivering an intuitive user experience, I leveraged my skills in React.js and Next.js to build robust functionalities while ensuring a seamless and visually appealing interface. Collaborating closely with the team, I contributed actively to code reviews and worked diligently to optimize the systems for enhanced performance and customer interaction. During period, Throughout this period, I deepened my expertise in both frontend and backend development, aligning these skill sets with our project objectives and surpassing user expectations. Additionally, I extensively utilized MySQL databases to store data in a scalable manner, contributing to efficient data management and facilitating project scalability.`
+    position: "Frontend/ Backend Developer",
+    company: "Solar T.S",
+    company_link: "",
+    project:
+      "Customer Relationship Management, Landing Page, Content Management System",
+    skills: "TypeScript, React.js, Next.js, Express, MySQL",
+    period: "May 2023 - November-2023",
+    desc: `Development of a comprehensive Customer Relationship Management (CRM) system, a captivating Landing Page, and a dynamic Content Management System (CMS). Employing TypeScript, React.js, Next.js, and Express, I played a pivotal role in crafting these solutions. With a focus on delivering an intuitive user experience, I leveraged my skills in React.js and Next.js to build robust functionalities while ensuring a seamless and visually appealing interface. Collaborating closely with the team, I contributed actively to code reviews and worked diligently to optimize the systems for enhanced performance and customer interaction. During period, Throughout this period, I deepened my expertise in both frontend and backend development, aligning these skill sets with our project objectives and surpassing user expectations. Additionally, I extensively utilized MySQL databases to store data in a scalable manner, contributing to efficient data management and facilitating project scalability.`,
   },
   {
-    "position": "Frontend/Backend Dev, Frelance Worker",
-    "company": " Fiverr",
-    "company_link": "",
-    "project": "Ecommerce Project, Database Management, scaling and integration",
-    "skills": "Nextjs, React.js, Zustand, Material UI, Tailwind, MySQL",
-    "period": "2023 - Present",
-    "desc": `As a Freelance Frontend/Backend Developer at Fiverr, I spearheaded an e-commerce project focusing on robust Database Management, Scaling, and seamless Integration with both frontend and backend. Leveraging Next.js, React.js Zustand, Material UI, Tailwind, and MySQL, I ensured a high-performing and scalable platform.  My emphasis has been on optimizing database performance, scalability, and smooth connectivity across all application layers while I kept improving my expertise on these topics.`
-  }
-]
+    position: "Frontend/Backend Dev, Frelance Worker",
+    company: " Fiverr",
+    company_link: "",
+    project: "Ecommerce Project, Database Management, scaling and integration",
+    skills: "Nextjs, React.js, Zustand, Material UI, Tailwind, MySQL",
+    period: "2023 - Present",
+    desc: `As a Freelance Frontend/Backend Developer at Fiverr, I spearheaded an e-commerce project focusing on robust Database Management, Scaling, and seamless Integration with both frontend and backend. Leveraging Next.js, React.js Zustand, Material UI, Tailwind, and MySQL, I ensured a high-performing and scalable platform.  My emphasis has been on optimizing database performance, scalability, and smooth connectivity across all application layers while I kept improving my expertise on these topics.`,
+  },
+];
 
 const Experience = () => {
   return (
@@ -43,7 +44,7 @@ const Experience = () => {
               </svg>
             </span>
             <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              {experience.company}: {experience.position} 
+              {experience.company}: {experience.position}
             </h3>
             <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
               {experience.period}
@@ -52,9 +53,7 @@ const Experience = () => {
               {experience.desc}
             </p>
           </li>
-
         ))}
-
       </ol>
     </section>
   );
